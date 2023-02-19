37eea401f69cbaf9509109d1a7382e171d90e363
yinpeng <y960303802@163.com>
2023-02-19 18:03:21 +0800

    [add] C++基础语法

---
+++ Acwing/算法语法课/AcWing 1. A + B.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/*
+    -*- coding: utf-8 -*-
+    @Author  : wheat
+    @Time    : 2023/02/10 22:07
+*/
+
+
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    cout << a + b << endl;
+    return 0;
+}
---
+++ Acwing/算法语法课/AcWing 608. 差.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/*
+    -*- coding: utf-8 -*-
+    @Author  : wheat
+    @Time    : 2023/02/10 22:09
+*/
+
+
+
+int main() {
+    int A, B, C, D;
+    cin >> A >> B >> C >> D;
+    cout << "DIFERENCA=" << A * B - C * D << endl;
+    return 0;
+}
